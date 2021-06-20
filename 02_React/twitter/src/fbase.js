1ea606f66410d761f40ff588dbc169300ee876c4
kimJiHwan0618 <wlghks0618@kakao.com>
2021-06-21 00:32:10 +0900

    twitter_ firestore 이용한 데이터 추가 구현

--- 02_React/twitter/src/fbase.js
+++ 02_React/twitter/src/fbase.js
@@ -1,19 +1,24 @@
 import firebase from "firebase/app";
-import "firebase/";
+import "firebase/auth";
+import "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
   appId: process.env.REACT_APP_API_ID,
-  measurementId: process.env.REACT_APP_MESSAGIN_ID,
+  // databaseURL: process.env.REACT_APP_DATABASE_URL,
+  // measurementId: process.env.REACT_APP_MESSAGIN_ID,
 };
 // REACT_APP_MEASUREMENT_Id=G-8GSGZQ44ST*/
 // REACT_APP_DATABASE_URL=https://myapp-project-123.firebaseio.com
 
 firebase.initializeApp(firebaseConfig);
 
+export const firebaseInstance = firebase;
+
 export const authService = firebase.auth();
+
+export const dbService = firebase.firestore();
--- 02_React/twitter/src/routes/Home.js
+++ 02_React/twitter/src/routes/Home.js
@@ -1,4 +1,35 @@
-import React from "react";
+import { dbService } from "fbase";
+import React, { useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [tweet, setTweet] = useState("");
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection("tweets").add({
+      tweet,
+      createdAt: Date.now(),
+    });
+    setTweet("");
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setTweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+          value={tweet}
+        />
+        <input type="submit" value="Tweet" />
+      </form>
+    </div>
+  );
+};
 export default Home;
