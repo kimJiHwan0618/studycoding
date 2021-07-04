da6939111eb27fb4e723fb5ad2ecbaa4d3df2385
kimJiHwan0618 <wlghks0618@kakao.com>
2021-07-04 13:01:09 +0900

    vanilla-redux__todo를 통한 state 예제 작성

--- 02_React/vanilla-redux/src/index.js
+++ 02_React/vanilla-redux/src/index.js
@@ -1,43 +1,84 @@
+/* eslint-disable default-case */
 import { createStore } from "redux";
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.getElementById("number");
+// const add = document.getElementById("add");
+// const minus = document.getElementById("minus");
+// const number = document.getElementById("number");
 
-const addNum = "addNum";
-const minusNum = "minusNum";
+// const addNum = "addNum";
+// const minusNum = "minusNum";
 
-const addCount = () => {
-  countStore.dispatch({ type: addNum });
-};
-const minusCount = () => {
-  countStore.dispatch({ type: minusNum });
-};
+// const addCount = () => {
+//   countStore.dispatch({ type: addNum });
+// };
+// const minusCount = () => {
+//   countStore.dispatch({ type: minusNum });
+// };
 
-add.addEventListener("click", addCount);
-minus.addEventListener("click", minusCount);
+// add.addEventListener("click", addCount);
+// minus.addEventListener("click", minusCount);
 
 // action 함수 store에 연결
-const countModifier = (count = 0, action) => {
+// const countModifier = (count = 0, action) => {
+//   switch (action.type) {
+//     case addNum:
+//       return count + 1;
+//     case minusNum:
+//       return count - 1;
+//     default:
+//       return count;
+//   }
+// };
+// const countStore = createStore(countModifier);
+// action 함수 store에 연결
+
+// state값 변화를 감지
+// const onchange = () => {
+//   number.innerText = countStore.getState();
+// };
+// countStore.subscribe(onchange);
+// state값 변화를 감지
+
+// 초기값 설정
+// countStore.dispatch({ type: "defaultNum" });
+// 초기값 설정
+
+// ↑↑↑ + , - 버튼을 통한 예제
+
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
+
+const reducer = (state = [], action) => {
+  // console.log(state);
   switch (action.type) {
-    case addNum:
-      return count + 1;
-    case minusNum:
-      return count - 1;
+    case ADD_TODO:
+      return [...state, { text: action.text, id: Date.now() }];
+    case DELETE_TODO:
+      return [];
     default:
-      return count;
+      return state;
   }
 };
-const countStore = createStore(countModifier);
-// action 함수 store에 연결
 
-// state값 변화를 감지
-const onchange = () => {
-  number.innerText = countStore.getState();
+const store = createStore(reducer);
+
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
+
+const createToDo = (toDo) => {
+  const li = document.createElement("li");
+  li.innerText = toDo;
+  ul.appendChild(li);
 };
-countStore.subscribe(onchange);
-// state값 변화를 감지
 
-// 초기값 설정
-countStore.dispatch({ type: "defaultNum" });
-// 초기값 설정
+const onSubmit = (e) => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  store.dispatch({ type: ADD_TODO, text: toDo });
+};
+
+store.subscribe(() => console.log(store.getState()));
+
+form.addEventListener("submit", onSubmit);
