0d46a73ded2d70a26a6e0047fc6a441db56b6c77
kimJiHwan0618 <wlghks0618@kakao.com>
2022-01-11 20:51:55 +0900

    drag and drop 연습

---
+++ 02_React/covid19/pages/api/example.js
@@ -0,0 +1,11 @@
+// import express from "express";
+// import cors from "cors";
+
+// const app = express();
+
+// const corsOptions = {
+//   origin: "http://openapi.data.go.kr/openapi/service/rest/Covid19",
+//   credentials: true,
+// };
+
+// export default app.use(cors(corsOptions));
